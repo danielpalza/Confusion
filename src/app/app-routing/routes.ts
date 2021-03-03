d31ecc4f3dff54e398aa7335d080906a5d722330
danielpalza <danielpalza@hotmail.com>
2021-03-03 18:22:26 -0300

    Single Page Applications Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -6,9 +6,11 @@ import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 
+// los path con un /:parametro sirven para pasar parametros a travez de la url al componente
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',  component: MenuComponent },
-  { path: 'contactus',     component: ContactComponent },
+  { path: 'dishdetail/:id', component: DishdetailComponent },
+  { path: 'contactus', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/menu/dishdetail/dishdetail.component.ts
+++ src/app/menu/dishdetail/dishdetail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../../shared/dish';
+import { DishService } from '../../services/dish.service';
+
+// Params y ActivatedRoute permiten usar los parametros pasados a travez de la url
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 
 
@@ -9,13 +14,24 @@ import { Dish } from '../../shared/dish';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-
-  @Input()
+  // @input permite usar los parametros pasados en la declaracion del componente en el html padre
+  //@Input()
   dish:Dish;
 
-  constructor() { }
+  //Se instancian los objetos de los servicios
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    // de route, se toma el parametro que se paso como /:id
+    const id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id.toString());
+  }
+  //Al usar Location, se puede pedir volver al elemento anterior en el historial al que estamos, volver a la
+  //anterior pagina
+  goBack(): void {
+    this.location.back();
   }
 
 }
