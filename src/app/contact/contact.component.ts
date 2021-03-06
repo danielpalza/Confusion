e5b67fa8a99a18baf2552d12faf75cccacb7f5e0
danielpalza <danielpalza@hotmail.com>
2021-03-06 16:07:21 -0300

    Reactive Forms Part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
 //FormBuilder permite crear un objeto que contenga los datos del formulario
 //y manejarlo directamente.
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -10,6 +11,10 @@ import { Feedback, ContactType } from '../shared/feedback';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+  //Se toma el valor del form que se estara validando
+  @ViewChild('fform') feedbackFormDirective;
+
 
   feedbackForm: FormGroup;
   feedback: Feedback;
@@ -24,12 +29,14 @@ export class ContactComponent implements OnInit {
 
   //Crea un objeto de formBuilder, que contenga los elementos que queremos 
   //ingresar en el formulario, y se guarda en el objeto de FormGroup, feedbackForm
+  // el array muestra primero el valor inicial, y segundo el metodo para validarlo, hay varios
+  //metodos
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', Validators.required ],
+      lastname: ['', Validators.required ],
+      telnum: ['', Validators.required ],
+      email: ['', Validators.required ],
       agree: false,
       contacttype: 'None',
       message: ''
@@ -37,10 +44,21 @@ export class ContactComponent implements OnInit {
   }
 
   //Accion que se realizara al clickear el boton submit del formulario
+  //El reset reestablecera los datos iniciales al resetear
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+    this.feedbackFormDirective.resetForm();
   }
+  
 
 }
