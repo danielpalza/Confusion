a53013734f8c213cd07c46b116be5821d71131f3
danielpalza <danielpalza@hotmail.com>
2021-03-11 13:23:13 -0300

    Reactive Forms Part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -20,6 +20,36 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   contactType = ContactType;
 
+  //Un objeto que contiene los errores presentes en el atributo
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  //Un objeto que contiene los errores y su tipo, de su determinado atributo
+  validationMessages = {
+    'firstname': {
+      'required':      'First Name is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required':      'Last Name is required.',
+      'minlength':     'Last Name must be at least 2 characters long.',
+      'maxlength':     'Last Name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required':      'Tel. number is required.',
+      'pattern':       'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email not in valid format.'
+    },
+  };
+
   constructor(private fb: FormBuilder) { 
     this.createForm();
   }
@@ -31,16 +61,25 @@ export class ContactComponent implements OnInit {
   //ingresar en el formulario, y se guarda en el objeto de FormGroup, feedbackForm
   // el array muestra primero el valor inicial, y segundo el metodo para validarlo, hay varios
   //metodos
-  createForm() {
+  createForm(): void {
+    //cada validator representa un tipo de control, todos deben estar encerrados en un array
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required ],
-      lastname: ['', Validators.required ],
-      telnum: ['', Validators.required ],
-      email: ['', Validators.required ],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      telnum: ['', [Validators.required, Validators.pattern] ],
+      email: ['', [Validators.required, Validators.email] ],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    //Chequea si hay valores cambiados gracias al observer valueChanges,
+    //si los hay, ejecuta onValueChanged
+    this.feedbackForm.valueChanges
+    .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+
   }
 
   //Accion que se realizara al clickear el boton submit del formulario
@@ -58,6 +97,27 @@ export class ContactComponent implements OnInit {
       message: ''
     });
     this.feedbackFormDirective.resetForm();
+  }
+  //Chequea si el atributo fue modificado, y si no es valido, cargara los errores 
+  //correspondientes
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
   
 
